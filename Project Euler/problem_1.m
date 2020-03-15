5323ad15a1d208ea8c84a10aba1b5580d11e403d
Rishikesh <62166003+rishikeshreddypatlolla@users.noreply.github.com>
2020-03-15 12:23:23 +0530

    Update problem_1.m

--- Project Euler/problem_1.m
+++ Project Euler/problem_1.m
@@ -1,8 +1,7 @@
->> %PROBLEM-1 
->> %If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-   %Find the sum of all the multiples of 3 or 5 below 1000.
->> 
->> i=1:999;
+ %PROBLEM-1 
+ %If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ %Find the sum of all the multiples of 3 or 5 below 1000.
+i=1:999;
 remainder_3 = rem(i,3);
 remainder_5 = rem(i,5);
 multiples_3 = i(remainder_3==0);
@@ -16,4 +15,4 @@ for k = 1:333
     end
 end
 totalsum = sum(multiples_3) + sum(multiples_5) - sum_repeated;
->>  disp(totalsum);
+disp(totalsum);
